dc268c207221af0fe57c1d5d466f3413f0deac0d
joe <joe.risner@proton.me>
2025-10-29 23:01:30 -0600

    Add bin/ci and fix files

--- src/dk/commands/sync.py
+++ src/dk/commands/sync.py
@@ -8,7 +8,7 @@ from ..utils import error, out, run_cmd
 
 
 def validate_config(config):
-    if config is None or 'sync' not in config or config['sync'] is None or 'repositories' not in config['sync']:
+    if config is None or "sync" not in config or config["sync"] is None or "repositories" not in config["sync"]:
         error("Missing required 'sync' configuration")
         out("\nAdd a 'sync' node to the config file with a list of repositories to sync. For example:\n")
         out(
@@ -23,10 +23,10 @@ def validate_config(config):
 
 
 def get_repos_from_config():
-    config = click.get_current_context().obj['config']
+    config = click.get_current_context().obj["config"]
     validate_config(config)
 
-    return config['sync']['repositories']
+    return config["sync"]["repositories"]
 
 
 def validate_directory(directory):
@@ -34,25 +34,25 @@ def validate_directory(directory):
         error(f"Could not find directory '{directory}'")
         sys.exit(1)
 
-    rev_parse_cmd = run_cmd(['git', '-C', directory, 'rev-parse'])
+    rev_parse_cmd = run_cmd(["git", "-C", directory, "rev-parse"])
 
     if rev_parse_cmd.returncode != 0:
-        error(f'{directory} is not a git project')
+        error(f"{directory} is not a git project")
         sys.exit(rev_parse_cmd.returncode)
 
 
 def sync_repository(repository_path):
-    git_status = run_cmd(['git', '-C', repository_path, 'status', '--porcelain'], return_output=True)
+    git_status = run_cmd(["git", "-C", repository_path, "status", "--porcelain"], return_output=True)
 
     if git_status:
-        out('Skipping this repo since there are local changes not yet committed', style='warn')
+        out("Skipping this repo since there are local changes not yet committed", style="warn")
         return
 
-    all_branches = run_cmd(['git', '-C', repository_path, 'remote', 'show', 'origin'], return_output=True)
-    default_branch = run_cmd(['awk', '/HEAD branch/ {print $NF}'], input=all_branches, return_output=True)
+    all_branches = run_cmd(["git", "-C", repository_path, "remote", "show", "origin"], return_output=True)
+    default_branch = run_cmd(["awk", "/HEAD branch/ {print $NF}"], input=all_branches, return_output=True)
 
-    run_cmd(['git', '-C', repository_path, 'checkout', default_branch], print_output=True)
-    run_cmd(['git', '-C', repository_path, 'pull'], print_output=True)
+    run_cmd(["git", "-C", repository_path, "checkout", default_branch], print_output=True)
+    run_cmd(["git", "-C", repository_path, "pull"], print_output=True)
 
 
 @click.command()
@@ -61,8 +61,8 @@ def sync():
     repositories = get_repos_from_config()
 
     for repository in repositories:
-        repository_path = Path.home() / 'projects' / repository
+        repository_path = Path.home() / "projects" / repository
 
-        out(f'==> Updating {repository}', style='highlight')
+        out(f"==> Updating {repository}", style="highlight")
         validate_directory(repository_path)
         sync_repository(repository_path)
--- src/dk/utils/rich_utils.py
+++ src/dk/utils/rich_utils.py
@@ -1,7 +1,7 @@
 from rich.console import Console
 from rich.theme import Theme
 
-theme = Theme({'error': 'bold red', 'success': 'bold green', 'warn': 'yellow', 'highlight': 'bold magenta'})
+theme = Theme({"error": "bold red", "success": "bold green", "warn": "yellow", "highlight": "bold magenta"})
 
 
 def out(str, style=None):
@@ -13,4 +13,4 @@ def out(str, style=None):
 def error(str):
     """Print stderr to the console using Rich."""
     console = Console(theme=theme, stderr=True)
-    console.print(f'[error]Error:[/error] {str}')
+    console.print(f"[error]Error:[/error] {str}")
